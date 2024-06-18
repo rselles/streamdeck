28ebf6865a450ccac141a094aedd290e478d4573
Rafael Sellés Vizcaíno <rafaelselles@gmail.com>
2024-06-18 13:33:49 +0200

    Create streamdeck.ino
    First merge of the two projects, pending testing on the actual hardware

---
+++ streamdeck.ino
@@ -0,0 +1,163 @@
+// Keyboard test at: https://www.keyboardtester.com/tester.html
+// To check sliders add a Serial Monitor to see the slider values in real time 
+
+// ---------------------------------
+// Key definitions
+#define BUTTON_KEY1 KEY_F13
+#define BUTTON_KEY2 KEY_F14
+#define BUTTON_KEY3 KEY_F15
+#define BUTTON_KEY4 KEY_F16
+#define BUTTON_KEY5 KEY_F17
+#define BUTTON_KEY6 KEY_F18
+#define BUTTON_KEY7 KEY_F19
+#define BUTTON_KEY8 KEY_F20
+
+// Pin definitions
+#define BUTTON_PIN1 2
+#define BUTTON_PIN2 3
+#define BUTTON_PIN3 4
+#define BUTTON_PIN4 5
+#define BUTTON_PIN5 6
+#define BUTTON_PIN6 7
+#define BUTTON_PIN7 8
+#define BUTTON_PIN8 9
+// ---------------------------------
+
+#include "Keyboard.h"
+
+// Button helper class for handling press/release and debouncing
+class button {
+  public:
+  const char key;
+  const uint8_t pin;
+
+  button(uint8_t k, uint8_t p) : key(k), pin(p){}
+
+  void press(boolean state){
+    if(state == pressed || (millis() - lastPressed  <= debounceTime)){
+      return; // Nothing to see here, folks
+    }
+
+    lastPressed = millis();
+
+    state ? Keyboard.press(key) : Keyboard.release(key);    
+    pressed = state;
+  }
+
+  void update(){
+    press(!digitalRead(pin));
+  }
+
+  private:
+  const unsigned long debounceTime = 30;
+  unsigned long lastPressed = 0;
+  boolean pressed = 0;
+} ;
+
+// Button objects, organized in array
+button buttons[] = {
+  {BUTTON_KEY1, BUTTON_PIN1},
+  {BUTTON_KEY2, BUTTON_PIN2},
+  {BUTTON_KEY3, BUTTON_PIN3},
+  {BUTTON_KEY4, BUTTON_PIN4},
+  {BUTTON_KEY5, BUTTON_PIN5},
+  {BUTTON_KEY6, BUTTON_PIN6},
+  {BUTTON_KEY7, BUTTON_PIN7},
+  {BUTTON_KEY8, BUTTON_PIN8},
+};
+
+const uint8_t NumButtons = sizeof(buttons) / sizeof(button);
+const uint8_t ledPin = 17;
+
+// -------------------------------
+// Sliders definitions
+const int NUM_SLIDERS = 4;
+const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3};
+
+int analogSliderValues[NUM_SLIDERS];
+
+void setup() {
+  setup_buttons();
+  setup_sliders();
+}
+
+void setup_buttons() { 
+  // Safety check. Ground pin #1 (RX) to cancel keyboard inputs.
+  pinMode(1, INPUT_PULLUP);
+  if(!digitalRead(1)){
+    failsafe();
+  }
+
+  // Set LEDs Off. Active low.
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  TXLED0;
+
+  for(int i = 0; i < NumButtons; i++){
+    pinMode(buttons[i].pin, INPUT_PULLUP);
+  }
+}
+
+void setup_sliders() { 
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    pinMode(analogInputs[i], INPUT);
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  loop_buttons();
+  loop_sliders();
+}
+
+void loop_buttons() {
+  for(int i = 0; i < NumButtons; i++){
+    buttons[i].update();
+  }
+}
+
+void loop_sliders() {
+  updateSliderValues();
+  sendSliderValues(); // Actually send data (all the time)
+  // printSliderValues(); // For debug
+  delay(10);
+}
+
+
+void failsafe(){
+  for(;;){} // Just going to hang out here for awhile :D
+}
+
+void updateSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+     analogSliderValues[i] = analogRead(analogInputs[i]);
+  }
+}
+
+void sendSliderValues() {
+  String builtString = String("");
+
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    builtString += String((int)analogSliderValues[i]);
+
+    if (i < NUM_SLIDERS - 1) {
+      builtString += String("|");
+    }
+  }
+  
+  Serial.println(builtString);
+}
+
+void printSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    String printedString = String("Slider #") + String(i + 1) + String(": ") + String(analogSliderValues[i]) + String(" mV");
+    Serial.write(printedString.c_str());
+
+    if (i < NUM_SLIDERS - 1) {
+      Serial.write(" | ");
+    } else {
+      Serial.write("\n");
+    }
+  }
+}
